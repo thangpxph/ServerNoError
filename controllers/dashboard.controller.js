91c1059de97957968fb587d47d6ff875383b5bb6
phamthang <you@example.com>
2020-11-18 00:56:35 +0700

    user

--- controllers/dashboard.controller.js
+++ controllers/dashboard.controller.js
@@ -7,7 +7,22 @@ const getDashboard = (req, res) => {
         user: req.user.toJSON(),
     });
 };
-
+const getUserMananger = async (req, res) => {
+    // Lấy thông tin người dùng từ mongodb.
+    let users = await User.find()
+        .select("-password") // Trả về tất cả thông tin trừ password sẽ không lấy
+        .lean();
+    let newData = users.map((item, index) => ({
+        ...item,
+        noNum: index + 1,
+    }));
+    res.render("users", {
+        title: "Quản lý người dùng",
+        layout: "dashlayout",
+        userList: newData,
+    });
+};
 module.exports = {
     getDashboard,
+    getUserMananger,
 };
--- routes/dashboard.route.js
+++ routes/dashboard.route.js
@@ -4,6 +4,7 @@ const router = express.Router();
 
 const AdminRoutes = (passport) => {
     router.get("/", dashboard.getDashboard);
+    router.get("/users", dashboard.getUserMananger);
     return router;
 };
 
